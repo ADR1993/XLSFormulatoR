39d0fd193f6374b7c6b8775580c1ddbbdf6da184
Augusto Dalla Ragione <54884671+ADR1993@users.noreply.github.com>
2024-06-20 15:38:09 +0200

    Add files via upload

--- R/kobo_to_edgelist.R
+++ R/kobo_to_edgelist.R
@@ -1,34 +1,34 @@
 #' kobo_to_edgelist function file.
 #'
-#' @param datafile The name of the XLS data object exported from KoboToolbox.
+#' @param path The name of the XLS data object exported from KoboToolbox.
+#' @param questions The labeled list used in the creation of the XLSForm.
 #' @param save If not NULL, then R was save the data with name "save".
 #' @return A data frame containing a network edgelist. 
 #' @export
 
- kobo_to_edgelist = function(datafile, save = NULL){
-  # Get names of excel sheets 
-  sheet_names = readxl::excel_sheets(datafile) 
-  repeats = stringr::str_which(sheet_names, "repeat") 
-  vec = vector(mode = "list", length = length(repeats)) 
+kobo_to_edgelist = function(path, questions, save = NULL){
+ # Re-create repeat group names
+ q_name = names(questions)
+ q_name_repeat = paste0(q_name, "_repeat")
 
-  # Loop 
-  for(i in 1:length(repeats)){ 
-  vec[[i]] = readxl::read_excel(datafile, sheet = repeats[i]) 
-  colnames(vec[[i]]) = c("note", "name", "image_source", "network_layer", "image", 
-                         "label", "id_display", "index", "parent_table_name", "parent_index", 
-                         "submission_id", "submission_uuid", "submission_time", "submission_validation_status", "submission_notes", 
-                         "submission_status", "submission_submitted_by", "submission_version", "submission_tags") } 
+ vec = vector(mode = "list", length = length(q_name))
 
-  # Bind together the elements of the list 
-  d = do.call(rbind, vec) 
+ for(i in 1:length(q_name_repeat)){ 
+  vec[[i]] = read_excel(path, sheet = q_name_repeat[i]) 
+  vec[[i]] = vec[[i]][colnames(vec[[i]]) %in% c(paste0(q_name[i], "_name"), paste0(q_name[i], "_id_display"))] 
+  vec[[i]]$layer = q_name[i] 
+  colnames(vec[[i]]) = c("alter", "focal", "layer") 
+   } 
 
-  # Rename columns and select 
-  d = dplyr::rename(d, alter = name, focal = id_display, layer = network_layer) 
-  d = dplyr::select(d, focal, alter, layer) 
+ # Bind together the elements of the list
+  d = do.call(rbind, vec) # Reorder the columns 
+  d = dplyr::relocate(d, focal, alter, layer) 
 
+ # Save if desired
   if(!is.null(save)){
     utils::write.csv(d,paste0(save,".csv"))
   }
-
-  return(d)
+  return(d) 
 }
+
+
