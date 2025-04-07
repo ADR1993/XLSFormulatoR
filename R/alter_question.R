fbba1b64568d6704f7a6cc409abf7454d1bd1a41
Augusto Dalla Ragione <54884671+ADR1993@users.noreply.github.com>
2025-04-07 16:07:13 +0200

    Add files via upload

---
+++ R/alter_question.R
@@ -0,0 +1,17 @@
+#' alter_question function file.
+#'
+#' @param prompt The text prompt that will be displayed in the app.
+#' @param type The question type. The function supports the following types: "text", "decimal", "select_one", "likert".
+#' @param options A vector of possible choices for question types "select_one" and "likert".
+#' @return A list containing the question prompt, the question type, and the list of options for "select_one" and "likert" questions.
+#' @export
+
+alter_question = function(prompt, type, options = NULL){
+  
+  if(!type %in% c("text", "decimal", "select_one", "likert")){
+    stop("Not a valid type. Valid types: text, decimal, select_one, likert")
+  }
+  
+  ls = list(prompt, type, options)
+  return(ls)
+}
--- R/focal_info.R
+++ R/focal_info.R
@@ -16,14 +16,14 @@ focal_info = function(filename, type){
   
   # Build XLSForm header
   colnames = c("type", "name", "label", "hint", "appearance", 
-               "relevant", "choice_filter", "calculation", "media::image")
-
+               "relevant", "choice_filter", "calculation", "media::image", "repeat_count")
+  
   start = c(rep("start", 2), rep(NA, times = length(colnames) - 2))
-
+  
   end = c(rep("end", 2), rep(NA, times = length(colnames) - 2))
-
+  
   today = c(rep("today", 2), rep(NA, times = length(colnames) - 2))
-
+  
   username = c(rep("username", 2), rep(NA, times = length(colnames) - 2))
   
   # Now build focal-info group that depends on external csv file
@@ -35,27 +35,26 @@ focal_info = function(filename, type){
   
   # Focal confirmation with picture
   begin_group2 = c("begin_group", 
-                    "focal_confirmation", 
-                    rep(NA, 2), 
-                    "field-list", 
-                    rep(NA, length(colnames) - 5))
+                   "focal_confirmation", 
+                   rep(NA, 2), 
+                   "field-list", 
+                   rep(NA, length(colnames) - 5))
   row1 = c("calculate", 
-            "focal_image_source", 
-            rep(NA, 5), 
-            paste0("concat(${focal_id}, '.", type, "')"), 
-            rep(NA, 1))
-  row2 = c("note", "focal_image", rep(NA, 6), "${focal_image_source}")
+           "focal_image_source", 
+           rep(NA, 5), 
+           paste0("concat(${focal_id}, '.", type, "')"), 
+           rep(NA, 2))
+  row2 = c("note", "focal_image", rep(NA, 6), "${focal_image_source}", rep(NA, 1))
   row3 = c("note", 
-            "focal_id_confirmation", 
-            "Confirm the identity of the interviewed person", 
-            rep(NA, length(colnames) - 3))
+           "focal_id_confirmation", 
+           "Confirm the identity of the interviewed person", 
+           rep(NA, length(colnames) - 3))
   end_group2 = c("end_group", "focal_confirmation", rep(NA, length(colnames) - 2))
   
   # Output
   table = rbind(start, end, today, username,
-                  begin_group1, arg1, end_group1,
-                  begin_group2, row1, row2, row3, end_group2)
+                begin_group1, arg1, end_group1,
+                begin_group2, row1, row2, row3, end_group2)
   colnames(table) = colnames
   return(table)
 }
-
