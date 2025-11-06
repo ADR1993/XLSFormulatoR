7d8629dcf54f37075da4cd36c6632417e984a070
Cody T. Ross <ross.cody.taylor@gmail.com>
2025-11-06 19:18:37 +0100

    Update layer_to_edgelist.R

--- R/layer_to_edgelist.R
+++ R/layer_to_edgelist.R
@@ -5,53 +5,32 @@
 #' @return A list containing a network edgelist and a hash list for out of roster individuals for the given network layer. 
 #' @export
 
-layer_to_edgelist = function(d, layer){
-  
-  #proceed if the dataframe is not empty
-  if( sum(is.na(d[,which(colnames(d) == paste0(layer, "_name"))][[1]])) != nrow(d) ){
-    
-    #create alter column
-    d$alter = rep(NA, nrow(d))
-    for(i in 1:nrow(d)){
-      if(d[[paste0(layer, "_name")]][i] == "out_of_roster"){
-        d$alter[i] = d[[paste0(layer, "_hash")]][i]
-      } else {
-        d$alter[i] = d[[paste0(layer, "_name")]][i]
-      }
+layer_to_edgelist = function (d, layer) = {
+    d = d %>% filter(!is.na(.data[[paste0(layer, "_name")]]))                          # Fix the small-bug coming from an NA row if the users goes back in Kobo
+
+    if(sum(is.na(d[, which(colnames(d) == paste0(layer, "_name"))][[1]])) != nrow(d)){ # WTF does this do?
+        d$alter = rep(NA, nrow(d))
+        d$focal = rep(NA, nrow(d))
+        d$layer = rep(NA, nrow(d))
+
+        for (i in 1:nrow(d)){                                                          # Simplify the branching logic
+            if (d[[paste0(layer, "_name")]][i] != "out_of_roster") {
+                d$focal[i] = d[[paste0(layer, "_id_display")]][i]
+                d$layer[i] = d[[paste0("network_layer_", layer)]][i]
+                d$alter[i] = d[[paste0(layer, "_hash")]][i]
+            }
+            else {
+                d$focal[i] = d[[paste0(layer, "_id_display_out")]][i]
+                d$layer[i] = d[[paste0("network_layer_", layer, "_out")]][i]
+                d$alter[i] = d[[paste0(layer, "_name")]][i]
+            }
+        }
+        edgelist = d[, c("focal", "alter", "layer")]
     }
-    
-    #create focal column
-    d$focal = rep(NA, nrow(d))
-    for(i in 1:nrow(d)){
-      if(d[[paste0(layer, "_name")]][i] != "out_of_roster"){
-        d$focal[i] = d[[paste0(layer, "_id_display")]][i]
-      } else {
-        d$focal[i] = d[[paste0(layer, "_id_display_out")]][i]
-      }
+    else {
+        edgelist = data.frame(focal = character(), alter = character(), 
+            layer = character())
     }
-    
-    #create layer column
-    d$layer = rep(NA, nrow(d))
-    for(i in 1:nrow(d)){
-      if(d[[paste0(layer, "_name")]][i] != "out_of_roster"){
-        d$layer[i] = d[[paste0("network_layer_", layer)]][i]
-      } else {
-        d$layer[i] = d[[paste0("network_layer_", layer, "_out")]][i]
-      }
-    }
-    
-    #create final edgelist
-    edgelist = d[,c("focal", "alter", "layer")]
-    
-  } else {
-    
-    #create empty edgelist if the original dataframe is empty
-    edgelist = data.frame(
-      focal = character(),
-      alter = character(),
-      layer = character()
-    )
-  }
-  
-  return(edgelist)
+    return(edgelist)
 }
+
